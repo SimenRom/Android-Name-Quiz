32c7cec27284af357c4643291cffcb04fe302662
Vaidios <25388414+Vaidios@users.noreply.github.com>
2020-02-16 23:37:24 +0100

    Added tests

--- app/src/main/java/com/example/thenamequiz/minApplication.java
+++ app/src/main/java/com/example/thenamequiz/minApplication.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -26,62 +27,99 @@ public class minApplication extends Application {
     public boolean firstTime = true;
     private SharedPreferences minePrefs;
 
+
+    public boolean isUsingSharedPreferenes = true;
+
     public minApplication(){
         super();
 
     }
-    public boolean fjern(Profil p){
-        Log.i("minTag", "prøver å fjerne " + p.getNavn());
-        boolean bleiFjerna = false;
-        if(!profiler.remove(p)){
-            Log.i("minTag", "Blei ikkje fjerna korrekt. Kan ikkje bruke denne metoden.");
-        }
-        minePrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Set<String> set = new HashSet<>(minePrefs.getStringSet("navneBase",  new HashSet<String>()));
-        String filnavn = "";
-        if(set == null){
-            Log.i("minTag", "Prøver å fjerne, men navneBase er null");
-        } else {
-            Iterator<String> it = set.iterator();
-            while(it.hasNext()){
-                filnavn = it.next();
-                if(filnavn.contains(p.getNavn())){
-                    set.remove(filnavn);
-                    if(deleteFromInternalStorage(filnavn)){
-                        Log.i("minTag", filnavn + " blei sletta. ");
-                        bleiFjerna = true;
-                    } else {
-                        Log.i("minTag", filnavn + " blei ikkje sletta. ");
-                    }
+
+    public boolean add(Profil newProfile) {
+        this.profiler.add(newProfile);
+        if (isUsingSharedPreferenes) {
+            Log.i("tag", "lagde ny profil. Legger den til...");
+            //legger til navnet i databasen som tar vare på alle navn og lagrer PB lokalt.
+            Set<String> set = new HashSet<>(minePrefs.getStringSet("navneBase", new HashSet<String>()));
+            int i = 0;
+            do {
+                i++;
+                Log.i("tag", "Forsøker å finne riktig nummer. i=" + i);
+                if(!set.contains(newProfile.getNavn() + "_" + i)){
+                    set.add(newProfile.getNavn() + "_" + i);
+                    this.saveToInternalStorage(newProfile.getBilde(), newProfile.getNavn(), i);
                     break;
                 }
-            }
-            Log.i("minTag", "strls set: " + set.size());
+            } while (true);
+            SharedPreferences.Editor myPrefsEditor = minePrefs.edit();
+            myPrefsEditor.putStringSet("navneBase", set).apply();
+            Log.i("tag: ", minePrefs.getStringSet("navneBase", null).toString());
         }
-        minePrefs.edit().putStringSet("navneBase", set).commit();
+
+        return true;
+    }
+    public boolean fjern(Profil p){
+
+
+        boolean bleiFjerna = false;
+        profiler.remove(p);
+
+        if (isUsingSharedPreferenes) {
+            minePrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            Set<String> set = new HashSet<>(minePrefs.getStringSet("navneBase",  new HashSet<String>()));
+            String filnavn = "";
+            if(set == null){
+                Log.i("minTag", "Prøver å fjerne, men navneBase er null");
+            } else {
+                Iterator<String> it = set.iterator();
+                while(it.hasNext()){
+                    filnavn = it.next();
+                    if(filnavn.contains(p.getNavn())){
+                        set.remove(filnavn);
+                        if(deleteFromInternalStorage(filnavn)){
+                            Log.i("minTag", filnavn + " blei sletta. ");
+                            bleiFjerna = true;
+                        } else {
+                            Log.i("minTag", filnavn + " blei ikkje sletta. ");
+                        }
+                        break;
+                    }
+                }
+                Log.i("minTag", "strls set: " + set.size());
+            }
+            minePrefs.edit().putStringSet("navneBase", set).commit();
+        } else {
+            bleiFjerna = true;
+        }
         return bleiFjerna;
 
     }
     public void populate(){
-        //legger til profiler fra storage
-        minePrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Set<String> set = new HashSet<>(minePrefs.getStringSet("navneBase",  new HashSet<String>()));
-        Log.i("debug: ", "Populate: set-strls: " + (set == null ? "null" : set.toString()));
-        if(set != null) {
-            for (String n : set) {
-                String[] filNavn = n.split("_");
-                //int nr = Integer.parseInt(filNavn[1].split(".")[0]);
-                profiler.add(new Profil(filNavn[0], loadImageFromStorage(filNavn[0], Integer.parseInt(filNavn[1]))));
+
+        if (isUsingSharedPreferenes) {
+            //legger til profiler fra storage
+            minePrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            Set<String> set = new HashSet<>(minePrefs.getStringSet("navneBase",  new HashSet<String>()));
+            Log.i("debug: ", "Populate: set-strls: " + (set == null ? "null" : set.toString()));
+            if(set != null) {
+                for (String n : set) {
+                    String[] filNavn = n.split("_");
+                    //int nr = Integer.parseInt(filNavn[1].split(".")[0]);
+                    profiler.add(new Profil(filNavn[0], loadImageFromStorage(filNavn[0], Integer.parseInt(filNavn[1]))));
+                }
             }
         }
-
-
         //legger til eksempel-profiler om det var ingen fra storage
         if (profiler.size() == 0){
-            Bitmap defaultBilde = BitmapFactory.decodeResource(getResources(), R.drawable.mario);
-            Bitmap defaultBilde2 = BitmapFactory.decodeResource(getResources(), R.drawable.luigi);
-            profiler.add(new Profil("Mario", defaultBilde));
-            profiler.add(new Profil("Luigi", defaultBilde2));
+            if (isUsingSharedPreferenes) {
+                Bitmap defaultBilde = BitmapFactory.decodeResource(getResources(), R.drawable.mario);
+                Bitmap defaultBilde2 = BitmapFactory.decodeResource(getResources(), R.drawable.luigi);
+                profiler.add(new Profil("Mario", defaultBilde));
+                profiler.add(new Profil("Luigi", defaultBilde2));
+            } else {
+                profiler.add(new Profil("Mario", null));
+                profiler.add(new Profil("Luigi", null));
+            }
 
         }
     }
